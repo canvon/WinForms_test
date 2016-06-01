1446356d655812768d92926f485eff912367dc6b
Fabian Pietsch <fabian@canvon.de>
2016-06-01 19:53:14 +0200

    Add LogUserControl, and use it in MainForm
    * Adds reference to System.Drawing, to get Point for use with Location
      properties.

---
+++ LogViewer/LogUserControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LogViewer
+{
+	public class LogUserControl : UserControl
+	{
+		protected Label LabelLogName;
+		protected ListBox ListBoxLog;
+		protected Button ButtonReadLog;
+
+		public LogUserControl()
+		{
+			// Create controls.
+			this.LabelLogName = new Label();
+			this.ListBoxLog = new ListBox();
+			this.ButtonReadLog = new Button();
+
+			//
+			// LabelLogName
+			//
+			this.LabelLogName.Name = "LabelLogName";
+			this.LabelLogName.Text = "Unnamed log";
+			this.LabelLogName.Location = new Point(
+				this.LabelLogName.Margin.Left,
+				this.LabelLogName.Margin.Top
+			);
+
+			//
+			// ListBoxLog
+			//
+			this.ListBoxLog.Name = "ListBoxLog";
+			this.ListBoxLog.Location = new Point(
+				this.ListBoxLog.Margin.Left,
+				this.LabelLogName.Bottom + this.LabelLogName.Margin.Bottom + this.ListBoxLog.Margin.Top
+			);
+
+			//
+			// ButtonReadLog
+			//
+			this.ButtonReadLog.Name = "ButtonReadLog";
+			this.ButtonReadLog.Location = new Point(
+				this.ButtonReadLog.Margin.Left,
+				this.ListBoxLog.Bottom + this.ListBoxLog.Margin.Bottom + this.ButtonReadLog.Margin.Top
+			);
+
+			//
+			// LogUserControl
+			//
+
+			// Add controls to user control.
+			this.Controls.Add(this.LabelLogName);
+			this.Controls.Add(this.ListBoxLog);
+			this.Controls.Add(this.ButtonReadLog);
+		}
+	}
+}
--- LogViewer/MainForm.cs
+++ LogViewer/MainForm.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace LogViewer
 {
 	public class MainForm : Form
 	{
+		protected LogUserControl LogUC1;
+
 		public MainForm()
 		{
+			// Create controls.
+			this.LogUC1 = new LogUserControl();
+
+			//
+			// LogUC1
+			//
+			this.LogUC1.Name = "LogUC1";
+			this.LogUC1.Location = new Point(
+				this.LogUC1.Margin.Left,
+				this.LogUC1.Margin.Top
+			);
+
+			//
+			// MainForm
+			//
+			this.Name = "MainForm";
 			this.Text = "LogViewer";
+
+			// Add controls to form.
+			this.Controls.Add(this.LogUC1);
 		}
 	}
 }
