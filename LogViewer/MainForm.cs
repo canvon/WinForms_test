471e0ebc6b89eba2554ef844784a50474f2d187e
Fabian Pietsch <fabian@canvon.de>
2016-06-01 19:21:08 +0200

    Add empty Form
    * Add references for System and System.Windows.Forms
    * Add Program which has a Main() and calls Application.Run()
      with the MainForm.
    * Add MainForm, which inherits from Form, sets a window title
      but is otherwise just empty.
    
    This compiles and runs and opens an empty window.

---
+++ LogViewer/MainForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LogViewer
+{
+	public class MainForm : Form
+	{
+		public MainForm()
+		{
+			this.Text = "LogViewer";
+		}
+	}
+}
---
+++ LogViewer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LogViewer
+{
+	public class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+
+			Application.Run(new MainForm());
+		}
+	}
+}
