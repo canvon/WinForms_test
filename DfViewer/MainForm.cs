b47720edc193deb6fea91446bfb36b17b0c4ba3f
Fabian Pietsch <fabian@canvon.de>
2016-10-04 23:22:16 +0200

    [df] Add a ListView, populate it with example data

--- DfViewer/MainForm.cs
+++ DfViewer/MainForm.cs
@@ -5,8 +5,49 @@ namespace DfViewer
 {
 	public class MainForm : Form
 	{
+		protected ListView listViewDfStatus;
+
+
 		public MainForm()
 		{
+			//
+			// listViewDfStatus
+			//
+			listViewDfStatus = new ListView();
+			listViewDfStatus.Name = "listViewDfStatus";
+			listViewDfStatus.Dock = DockStyle.Fill;
+			listViewDfStatus.Columns.Add("Path");
+			listViewDfStatus.Columns.Add("Size");
+			listViewDfStatus.Columns.Add("Used");
+			listViewDfStatus.Columns.Add("Free");
+			listViewDfStatus.Columns.Add("Beam");
+			listViewDfStatus.View = View.Details;
+
+			//
+			// MainForm
+			//
+			this.Name = "MainForm";
+			this.Text = "df viewer";
+
+			this.Controls.Add(listViewDfStatus);
+
+
+			LoadDfStatuses();
+		}
+
+
+		public void LoadDfStatuses()
+		{
+			listViewDfStatus.Items.Clear();
+			listViewDfStatus.Items.Add(new ListViewItem(new string[] {
+				"/", "500000", "400000", "100000", string.Empty
+			}));
+			listViewDfStatus.Items.Add(new ListViewItem(new string[] {
+				"/dev", "10", "1", "9", string.Empty
+			}));
+			listViewDfStatus.Items.Add(new ListViewItem(new string[] {
+				"/boot", "500", "200", "300", string.Empty
+			}));
 		}
 	}
 }
