d007b361220c7040edfb69e6e22b461d6c64c7e8
Fabian Pietsch <fabian@canvon.de>
2016-10-05 00:04:32 +0200

    [df] Add class DfStatus; tag items, draw beams accordingly
    So now every beam graph looks different, according to devised data.

---
+++ DfViewer/DfStatus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DfViewer
+{
+	public class DfStatus
+	{
+		public string FilesystemPath {
+			get;
+			private set;
+		}
+
+		public long DfSize {
+			get;
+			private set;
+		}
+
+		public long DfUsed {
+			get;
+			private set;
+		}
+
+		public long DfFree {
+			get;
+			private set;
+		}
+
+
+		public DfStatus(string filesystemPath, long dfSize, long dfUsed, long dfFree)
+		{
+			this.FilesystemPath = filesystemPath;
+			this.DfSize = dfSize;
+			this.DfUsed = dfUsed;
+			this.DfFree = dfFree;
+		}
+	}
+}
--- DfViewer/MainForm.cs
+++ DfViewer/MainForm.cs
@@ -43,16 +43,24 @@ namespace DfViewer
 
 		public void LoadDfStatuses()
 		{
+			var dfStatuses = new DfStatus[] {
+				new DfStatus("/", 500000, 400000, 100000),
+				new DfStatus("/dev", 10, 1, 9),
+				new DfStatus("/boot", 500, 200, 300),
+			};
+
 			listViewDfStatus.Items.Clear();
-			listViewDfStatus.Items.Add(new ListViewItem(new string[] {
-				"/", "500000", "400000", "100000", string.Empty
-			}));
-			listViewDfStatus.Items.Add(new ListViewItem(new string[] {
-				"/dev", "10", "1", "9", string.Empty
-			}));
-			listViewDfStatus.Items.Add(new ListViewItem(new string[] {
-				"/boot", "500", "200", "300", string.Empty
-			}));
+			foreach (DfStatus status in dfStatuses) {
+				var item = new ListViewItem(new string[] {
+					status.FilesystemPath,
+					status.DfSize.ToString(),
+					status.DfUsed.ToString(),
+					status.DfFree.ToString(),
+					string.Empty
+				});
+				item.Tag = status;
+				listViewDfStatus.Items.Add(item);
+			}
 		}
 
 		void ListViewDfStatus_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
@@ -80,9 +88,19 @@ namespace DfViewer
 			e.Graphics.FillRectangle(Brushes.Blue, e.Bounds);
 			e.Graphics.FillRectangle(Brushes.Aqua, inner);
 
-			Rectangle amount = inner;
-			amount.Width = inner.Width * 400000 / 500000;
-			e.Graphics.FillRectangle(Brushes.White, amount);
+			var status = e.Item.Tag as DfStatus;
+			if (!object.ReferenceEquals(status, null)) {
+				Rectangle amount = inner;
+				if (status.DfSize == 0) {
+					// Indicate something like an error.
+					e.Graphics.FillRectangle(Brushes.Red, amount);
+				}
+				else {
+					// Draw the actual amount, as a white bar.
+					amount.Width = (int)(inner.Width * status.DfUsed / status.DfSize);
+					e.Graphics.FillRectangle(Brushes.White, amount);
+				}
+			}
 		}
 	}
 }
