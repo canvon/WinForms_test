02b5be272a94c1b31cb44bd76e7314dcc0ab5e49
Fabian Pietsch <fabian@canvon.de>
2016-10-04 23:37:08 +0200

    [df] Set up ListView.OwnerDraw
    With this, we can draw our own beam graph! \o/

--- DfViewer/MainForm.cs
+++ DfViewer/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace DfViewer
@@ -16,6 +17,7 @@ namespace DfViewer
 			listViewDfStatus = new ListView();
 			listViewDfStatus.Name = "listViewDfStatus";
 			listViewDfStatus.Dock = DockStyle.Fill;
+			listViewDfStatus.OwnerDraw = true;
 			listViewDfStatus.Columns.Add("Path");
 			listViewDfStatus.Columns.Add("Size");
 			listViewDfStatus.Columns.Add("Used");
@@ -32,6 +34,9 @@ namespace DfViewer
 			this.Controls.Add(listViewDfStatus);
 
 
+			listViewDfStatus.DrawColumnHeader += ListViewDfStatus_DrawColumnHeader;
+			listViewDfStatus.DrawItem += ListViewDfStatus_DrawItem;
+			listViewDfStatus.DrawSubItem += ListViewDfStatus_DrawSubItem;
 			LoadDfStatuses();
 		}
 
@@ -48,6 +53,32 @@ namespace DfViewer
 			listViewDfStatus.Items.Add(new ListViewItem(new string[] {
 				"/boot", "500", "200", "300", string.Empty
 			}));
+		}
+
+		void ListViewDfStatus_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+		{
+			e.DrawDefault = true;
+		}
+
+		void ListViewDfStatus_DrawItem(object sender, DrawListViewItemEventArgs e)
+		{
+			//e.DrawDefault = true;  // This skips drawing the beam. :( So..:
+			e.DrawBackground();
+			e.DrawText();
+			e.DrawFocusRectangle();
+		}
+
+		void ListViewDfStatus_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+		{
+			if (e.ColumnIndex != 4) {
+				e.DrawDefault = true;
+				return;
+			}
+
+			// Draw a beam ourselves.
+			Rectangle rect = e.Bounds;
+			e.Graphics.FillRectangle(Brushes.Blue, rect);
+			e.Graphics.FillRectangle(Brushes.Aqua, Rectangle.Inflate(rect, -3, -3));
 		}
 	}
 }
